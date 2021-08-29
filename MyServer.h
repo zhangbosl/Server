065333399b43abf74126b17bdeb91a7197e2ac44
zhangbosl <1003326894@qq.com>
2021-08-29 09:09:46 +0800

    test

--- MyServer.h
+++ MyServer.h
@@ -9,18 +9,10 @@
 #include <arpa/inet.h>
 #include <vector>
 #include <string>
+#include "FD.h"
+#include "WorkData.h"
+
 struct sockaddr_in InitSock();
 int InitBind(int,sockaddr_in &);
 
-class FD
-{
-public:
-	std::vector<std::pair<int,std::string>>clifd;
-	int maxfd = 0; // allset中最大的fd
-	int lisfd;
-	fd_set allset;
-	void set(int);   //把fd放入allset
-	void zero();   //清空
-};
-void WorkData(int,int ,FD &);
 #endif
